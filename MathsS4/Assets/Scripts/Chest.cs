451791c100ac2513d0c861b376c5d44b2596fb57
Clemence <clemence.thierry60@orange.fr>
2022-03-20 16:41:10 +0100

    Add player movements and chest opening

---
+++ MathsS4/Assets/Scripts/Chest.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    private bool isInRange = false;
+
+    public Animator animator;
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isInRange)
+        {
+            OpenChest();
+            Debug.Log("Tu as gagné "+nbCoinWin()+" pièces.");
+        }
+        
+    }
+
+    void OpenChest()
+    {
+        animator.SetTrigger("OpenChest");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isInRange = false;
+        }
+    }
+
+    private int nbCoinWin()
+    {
+       double nb = Random.Range(0f, 1f);
+
+        if (nb <= 0.6)
+        {
+            return 1;
+        }
+        else
+        {
+            return 3;
+        }
+    }
+}
---
+++ MathsS4/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    private bool isJumping;
+    private bool isGrounding;
+
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
+
+    public Rigidbody2D rigidBodyPlayer;
+    private Vector3 velocity = Vector3.zero;
+
+    void FixedUpdate()
+    {
+        isGrounding = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        if (Input.GetButtonDown("Jump") && isGrounding) //correspond à la barre espace
+        {
+            isJumping = true;
+        }
+
+        MovePlayer(horizontalMovement);
+    }
+
+    void MovePlayer(float _horizontalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, rigidBodyPlayer.velocity.y);
+        rigidBodyPlayer.velocity = Vector3.SmoothDamp(rigidBodyPlayer.velocity, targetVelocity, ref velocity, .05f);
+
+        if (isJumping)
+        {
+            rigidBodyPlayer.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }
+    }
+}
